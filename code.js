b021d03548e411f301d375534044a6f5a1c0724e
businesswebsites <droese.websites@gmail.com>
2022-12-01 10:37:54 +0100

    Version 3 with special symbols

--- code.js
+++ code.js
@@ -20,17 +20,46 @@ function delete_sym(){
     console.log(str);
 }
 
-let symbols = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "ß", "q", "w", "e", "r", "t", "z", "u", "i", "o", "p", "ü", "a", "s", "d", "f", "g", "h", "j", "k", "l", "ö", "ä", "y", "x", "c", "v", "b", "n", "m", ",", ".", "-"];
+let symbols = ["q", "w", "e", "r", "t", "z", "u", "i", "o", "p", "ü", "a", "s", "d", "f", "g", "h", "j", "k", "l", "ö", "ä", "y", "x", "c", "v", "b", "n", "m"];
 let bigger_value = false;
+let special_symbols = [",", ".", "-", "ß"];
+let speical_symbols_2 = [";", ":", "_", "?"];
+let numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+let numbers_2 = ["!", '"', "§", "$",  "%", "&", "/", "(", ")", "="];
 
 function bigger(){
     if(bigger_value===false){
         bigger_value = true;
         document.getElementById("bigger_symbol").style.backgroundColor = "dimgrey";
 
+
+        for(let a = 0; a < symbols.length; a++){
+            document.getElementById(symbols[a]).innerHTML = symbols[a].toUpperCase();
+        }
+        for(let b = 0; b < special_symbols.length; b++){
+            document.getElementById(special_symbols[b]).innerHTML = speical_symbols_2[b];
+        }
+
+        for(let c = 0; c < numbers.length; c++){
+            document.getElementById(numbers[c]).innerHTML = numbers_2[c];
+        }
+
+
+
+
     }else{
         bigger_value = false;
         document.getElementById("bigger_symbol").style.backgroundColor = "lightgrey";
+        for(let a = 0; a < symbols.length; a++){
+            document.getElementById(symbols[a]).innerHTML = symbols[a].toLowerCase();
+        }
+        for(let b = 0; b < special_symbols.length; b++){
+            document.getElementById(special_symbols[b]).innerHTML = special_symbols[b];
+        }
+
+        for(let c = 0; c < numbers.length; c++){
+            document.getElementById(numbers[c]).innerHTML = numbers[c];
+        }
     }
     console.log(bigger_value)
 }
